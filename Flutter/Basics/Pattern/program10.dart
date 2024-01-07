d4836c03db9ba9efb6425efdd478789da5134e6a
harshadanbhule <harshadanbule007@gmail.com>
2024-01-08 03:08:07 +0530

    Dart

---
+++ Flutter/Basics/Pattern/program10.dart
@@ -0,0 +1,18 @@
+/*rows = 4
+$ $ $ $
+$ $ $
+$ $
+$*/
+import'dart:io';
+void main(){
+	int rows=4;
+	for(int i=rows;i>0;i--){
+		for(int j=i;j>0;j--){
+			stdout.write("\$");
+		}
+       
+        print("");
+	}
+
+
+}
